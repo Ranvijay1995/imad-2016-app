e16b53b7690c43b50056f5f617cc0e4960391c1a
Ranvijay1995 <kumarranvijay1307@gmail.com>
2016-10-27 08:25:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config = {
+  user: 'ranvijay1995',
+  database: 'ranvijay1995',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -15,6 +24,23 @@ app.get('/', function (req, res) {
 app.get('/profile', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
+
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM test', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+
+
 
 app.get('/blog', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
