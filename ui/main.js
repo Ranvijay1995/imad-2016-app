9612f6511ce7d4e89e6526f34bba683f598474fc
Ranvijay1995 <kumarranvijay1307@gmail.com>
2016-11-05 18:42:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,41 @@
-console.log('Loaded!');
+  
+  
+  
+  
+var submit = document.getElementById('login_btn');
+submit.onclick = function () {
+    
+    var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('Logged in successfully');
+              } else if (request.status === 403) {
+                  alert('username/password is invalid');
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+              } else {
+                  alert('Something went wrong on the server');
+              }
+          }  
+          // Not done yet
+        };
+};
+  
+  
+  
+  
+  
+  
+  
+  
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
