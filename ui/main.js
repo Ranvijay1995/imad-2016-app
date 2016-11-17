7d7d2ad807be3f61e1d85715799825b8ecffef4b
Ranvijay1995 <kumarranvijay1307@gmail.com>
2016-11-17 18:55:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,31 @@
 
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    // make a request to the server and send the name
+   //create a request object
+    var request= new XMLHttpRequest();
     
-    var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('Logged in successfully');
-              } else if (request.status === 403) {
-                  alert('username/password is invalid');
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-              } else {
-                  alert('Something went wrong on the server');
-              }
-          }  
-          // Not done yet
-        };
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));
+    //capture response and store it
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                console.log("user logged in");
+                alert("Logged in Succesfully");
+            }else if(request.status===403) {
+                alert("The Username?password is incorrect");
+            }else if(request.status===500) {
+                alert("Something Went Wrong on the Server");
+            }
+        }
+        // not done yet
+    };
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    //Make a request
+    request.open("POST","http://kshitijsrivastava.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
 };
