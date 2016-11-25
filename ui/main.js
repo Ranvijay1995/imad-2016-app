711a989076214dbbb9a9e55544cd439f4d9d43a5
Ranvijay1995 <kumarranvijay1307@gmail.com>
2016-11-25 20:12:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,4 +28,5 @@ submit.onclick=function(){
     request.open("POST","http://ranvijay1995.imad.hasura-app.io/login",true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
+    submit.value = 'Logging in...';
 };
